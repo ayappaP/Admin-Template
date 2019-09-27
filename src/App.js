73c21a3a7a1039429f257b883fc5ad09fe148615
Logeshdas <logeshdas21@gmail.com>
2019-09-27 18:41:40 +0530

    Convert to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component, Suspense } from "react";
 import { connect } from "react-redux";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Switch,
   Redirect
